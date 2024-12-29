d477d173dd7a1e0c6bbab10572dec84c4442c9ea
Amalakanti Shiva teja <shivateja.amalakanti@gmail.com>
2024-12-30 02:04:22 +0530

    Navbar tags working

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,25 @@
-const Header =()=>{
+const Header = () => {
     return (
-        <header className="fixed top-0 left-0 w-full h-20 flex items-center z-40 
-        bg-gradient-to-b from-zinc-900 to-zinc-900/0">
-            <div className="max-w-screen-2xl w-full mx-auto px-4">
-                <h1>
-                <a href="/" className="logo">
-                    <img src="/images/logo.png
-                    " width={40} height={40} alt="shiva teja"/>
-                    </a>
-                </h1>
-                
-            </div>
-
-        </header>
+      <header className="fixed top-0 left-0 w-full h-20 z-40 bg-gradient-to-b from-zinc-900 to-zinc-900/0">
+        <div className="max-w-screen-2xl w-full mx-auto px-4 flex justify justify-around items-center
+         md:px-6 md:grid md:grid-cols-[1fr,3fr,1fr]">
+          <h1>
+            <a href="/" className="logo">
+              <img src="/images/logo.png" width={40} height={40} alt="shiva teja" />
+            </a>
+          </h1>
+          <div className="relative md:justify-self-center">
+            <button onClick={null}
+             className="menu-btn md:hidden">
+              <span className="material-symbols-rounded">menu</span>
+            </button>
+            Navbar
+          </div>
+          <a href="#contact" className="">Contact Me</a>
+        </div>
+      </header>
     );
   }
   
   export default Header;
+  
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,54 @@
+// node modules
+import {useRef} from "react";
+
+
+const Navbar =()=>{
+    const lastActiveLink = useRef();
+    const activeBox = useRef();
+    const navItems = [
+        {
+          label: 'Home',
+          link: '#home',
+          className: 'nav-link active',
+          ref: lastActiveLink
+        },
+        {
+          label: 'About',
+          link: '#about',
+          className: 'nav-link'
+        },
+        {
+          label: 'Work',
+          link: '#work',
+          className: 'nav-link'
+        },
+        {
+          label: 'Reviews',
+          link: '#reviews',
+          className: 'nav-link'
+        },
+        {
+          label: 'Contact',
+          link: '#contact',
+          className: 'nav-link md:hidden'
+        }
+      ];
+    return(
+       <nav className={navbar}>
+{
+    navItems.map(({label,link,className,ref},key)=>
+        (
+        <a 
+            href={link}
+            key={key}
+            ref={ref}
+            className={className}
+            onClick={null}>{label}</a>
+    ))
+}   
+<div className="active-box" ref={activeBox}></div>
+       </nav>
+    );
+}
+
+export default Navbar;
