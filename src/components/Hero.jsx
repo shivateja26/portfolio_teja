e8d56ea83021370e67a3baf36de8854746d40647
Amalakanti Shiva teja <shivateja.amalakanti@gmail.com>
2024-12-31 21:31:22 +0530

    Changes

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -23,6 +23,7 @@ const Hero=()=>{
                 <div className="flex items-center gap-3">
                    <ButtonPrimary 
                    label="Resume" 
+                   href="/images/resume.pdf"
                    icon="download"/>
 
                    <ButtonOutline href="#about" label="Scroll down" icon="arrow_downward"/>
