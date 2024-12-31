3c59bfb4170114d7148a08612e0d03cde418b5da
Amalakanti Shiva teja <shivateja.amalakanti@gmail.com>
2024-12-31 14:44:29 +0530

    burger menu

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,31 +1,33 @@
-// Node components
 import { useState } from "react";
-// node modules
 import Navbar from "./Navbar";
 
 const Header = () => {
-    const [navOpen,setNavOpen]=useState(false);
-    return (
-      <header className="fixed top-0 left-0 w-full h-20 z-40 bg-gradient-to-b from-zinc-900 to-zinc-900/0">
-        <div className="max-w-screen-2xl w-full mx-auto px-4 flex justify justify-between items-center
-         md:px-6 md:grid md:grid-cols-[1fr,3fr,1fr]">
-          <h1>
-            <a href="/" className="logo">
-              <img src="/images/logo.png" width={40} height={40} alt="shiva teja" />
-            </a>
-          </h1>
-          <div className="relative md:justify-self-center">
-            <button onClick={null}
-             className="menu-btn md:hidden">
-              <span className="material-symbols-rounded">menu</span>
-            </button>
-            <Navbar navOpen={navOpen}/>
-          </div>
-          <a href="#contact" className="btn btn-secondary max-md:hidden md:justify-self-end">Contact Me</a>
+  const [navOpen, setNavOpen] = useState(false);
+
+  const handleMenuClick = () => {
+    setNavOpen(prevState => !prevState); 
+  };
+
+  return (
+    <header className="fixed top-0 left-0 w-full h-20 z-40 bg-gradient-to-b from-zinc-900 to-zinc-900/0">
+      <div className="max-w-screen-2xl w-full mx-auto px-4 flex justify-between items-center md:px-6 md:grid md:grid-cols-[1fr,3fr,1fr]">
+        <h1>
+          <a href="/" className="logo">
+            <img src="/images/logo.png" width={40} height={40} alt="shiva teja" />
+          </a>
+        </h1>
+        <div className="relative md:justify-self-center">
+          <button onClick={handleMenuClick} className="menu-btn md:hidden">
+            <span className="material-symbols-rounded">{navOpen ? 'close' : 'menu'}</span>
+          </button>
+          <Navbar navOpen={navOpen} />
         </div>
-      </header>
-    );
-  }
-  
-  export default Header;
-  
+        <a href="#contact" className="btn btn-secondary max-md:hidden md:justify-self-end">
+          Contact Me
+        </a>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
