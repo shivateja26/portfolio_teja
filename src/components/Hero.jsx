283ae2fd6de81a71da7a859ed9a2bc3d1c0b6778
Amalakanti Shiva teja <shivateja.amalakanti@gmail.com>
2024-12-31 18:59:19 +0530

    Hero section updation

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,16 +5,16 @@ const Hero=()=>{
             <div>
                 <div className="flex items-center gap-3">
                     <figure className="img-box w-9 h-9 rounded-lg">
-                        <img src="/images/avatar-1.png" width={40} height={40} alt="img-cover"/>
+                        <img src="/images/avatar-1.png" width={40} height={40} atlt="teja-potrait" className="img-cover"/>
                     </figure>
-                    <div className="">
-                        <span className="">
-                            <span className=""></span>
+                    <div className=" flex items-center gap-1.5 text-zinc-400 text-sm tracking-wide">
+                        <span className="relative w-2 h-2 rounded-full bg-emerald-400">
+                            <span className="absolute inset-0 rounded-full bg-emerald-400 animate-ping"></span>
                         </span>
                         Available
                     </div>
                 </div>
-                <h2>
+                <h2 className="headline-1">
                 I’m a Front-End Developer crafting seamless experiences
                 </h2>
                 <div className="">
