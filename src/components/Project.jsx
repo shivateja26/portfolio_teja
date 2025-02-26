4b75a1ab372f7be8d799fdcaeafa0b62ea3b88f2
Amalakanti Shiva teja <shivateja.amalakanti@gmail.com>
2025-02-27 00:08:20 +0530

    change

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -6,7 +6,7 @@ const Project = () => {
     { title: "Authentication-Mern", description: "Features : Email Verificaion,Password Recovery,Verification Email,Forgotpassword,Login And logout",link:"https://github.com/shivateja26/Authentication_mern.git" },
     { title: "Weather App", description: "Weather App with React JS and OpenWeatherMap API .", link: "https://github.com/shivateja26/weather_app" },
     { title: "TodoList", description: "users can add, mark as complete, and delete tasks. It uses React's useState for state management and event handlers to update the task list dynamically.", link: "https://github.com/shivateja26/todolist" },
-    { title: "Travelling", description: "A travel website built with React.js, offering destination exploration, trip bookings, and local attractions with a seamless, interactive experience.", link: "https://github.com/shivateja26/tourism" },
+    { title: "Spendix", description: "Developed a secure personal finance app using the MERN stack with JWT authentication. Features include income/expense tracking, visual charts (Recharts), and data export to Excel. Implemented user-friendly cards for quick access and management, responsive design for all devices, and sidebar navigation for seamless UX.", link: "https://github.com/shivateja26/spendix" },
     
     { title: "Project", description: "A mobile app that enhances user engagement.", link: "#" },
     { title: "Project", description: "A platform to connect people in new ways.", link: "#" },
