fd630e705a8d4181ea7afcdf7eb0da1cfde39daf
Amalakanti Shiva teja <shivateja.amalakanti@gmail.com>
2025-05-16 23:45:31 +0530

    Changes

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -16,8 +16,7 @@ const About = ()=>{
         <div className="container">
             <div className="bg-zinc-800/50 p-7 rounded-2xl md:p-12">
                 <p className="text-zinc-300 mb-4 md:mb-8 md:text-xl md:max-w-[80ch]">
-                Welcome!, I&apos;m Shiva Teja Amalakanti, a passionate Front-End Developer with over 3 years of hands-on experience in crafting seamless, high-performance websites and web applications. With a strong foundation in HTML, CSS, JavaScript, and React, I bring ideas to life with clean, responsive, and engaging user interfaces.I excel at tackling complex challenges and creating solutions that prioritize user experience, performance, and accessibility.
-                 I’ve collaborated with cross-functional teams, and delivered successful projects that not only meet client goals but also push the boundaries of creativity and functionality. I am eager to continue expanding my skill set, and I’m always excited about new challenges that allow me to grow as a developer.
+                Welcome!, I&apos;m Shiva Teja Amalakanti, Expertise in HTML, CSS, JavaScript, ReactJS, and Tailwind CSS. Proficient in building responsive, user-friendly, and visually appealing web applications. Experienced in leveraging Node.js for backend and enhancing full-stack capabilities. Adept at creating dynamic and interactive user interfaces while ensuring optimal performance and cross-browser compatibility. Continuously learning and staying updated with the latest industry trends
                  <br/>  <br/>Let’s connect — I’m always open to opportunities to collaborate and make a meaningful impact.
                 </p>
                 <div className="flex flex-wrap items-center gap-4 md:gap-7">{
